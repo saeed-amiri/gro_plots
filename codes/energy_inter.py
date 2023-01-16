7b117c3e82bddad964529e169b031bf30b8478e3
Saeed Amiri <samiri@uni-bremen.de>
2023-01-16 11:51:12 +0100

    initiate

---
+++ codes/energy_inter.py
@@ -0,0 +1,13 @@
+import sys
+import pandas as pd
+import typing
+
+class Doc:
+    """plot energy output of GROMACS from command li:
+    `gmx_mpi energy -f npt_rerun.edr -o XXXX.xvg`
+    Input:
+    Any number of input file with xvg extensions
+    """
+
+
+
